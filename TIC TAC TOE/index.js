869055e12c4c8d86e189d4bbc018ad3d290cbe1a
Muhammad JUNAID <junaidwattoo33@gmail.com>
2024-06-16 12:57:23 -0700

    Commited

--- TIC TAC TOE/index.js
+++ TIC TAC TOE/index.js
@@ -1,13 +1,14 @@
+`"use strict  ;"`
 let boxes=document.querySelectorAll(".box");
-let resetbtn=document.getElementsByClassName("reset-button");
+// let resetbtn=document.getElementsByClassName("reset-button");
+let resetbtn= document.querySelector(".reset-button");
 let newbtn=document.querySelector(".new-button");
 let msgcon=document.querySelector(".msg-container")
-// let msgp=document.querySelector("#msg");
 let msgp=document.getElementById("msg");
-let turn0=false;
+let turn0=true;
 const winpatterns=[
     [0,1,2],
-    [0.3,6],
+    [0,3,6],
     [0,4,8],
     [1,4,7],
     [2,5,8],
@@ -16,38 +17,86 @@ const winpatterns=[
     [6,7,8],
 ];
 boxes.forEach((box)=>{
-    box.addEventListener("click", ()=> {
+    box.addEventListener('click', ()=> {
 if(turn0){
-    box.innerHTML="0";
+    box.innerText="0";
+    box.classList.add("zero");
+    
     turn0=false;
 }
 else{
-    box.innerHTML="X";
+    box.innerText="X";
+    box.classList.add("cross");
     turn0=true;
 }
 box.disabled=true;
+// boxes.disabled=true;
+// boxes.classList.add("disabled");
+// box.classList.add("disabled"); 
+
 checkwinner();
     });
 })
-const showwinner = (winner) => {
-    // function showwinner(){
-document.getElementById("msg").innerText=`Congratulation Winner is ${winner}`;
-msgcon.classList.remove("hide");
-}
-const checkwinner = () =>{
-    // function checkwinner(winner){
-    for( let pattern of winpatterns){
-        console.log(boxes[pattern[0]].innerText,boxes[pattern[1]].innerText,boxes[pattern[2]].innerText);
+newbtn.addEventListener('click', ()=>{
+    location.reload();
+})
+resetbtn.addEventListener('click', ()=>{
+    location.reload();
+})
+const showwinner = (pos2value) => {
+    
+// let boxes=document.querySelectorAll(".box");
+
+      document.querySelector("#msg").innerText=`Winner is ${pos2value}`;
+      msgcon.classList.remove("hide");
+      boxes.forEach((box) => {
+        box.disabled = true;
+    });
+
   
-    let pos1value =      boxes[pattern[0]].innerText;
-    let pos2value =      boxes[pattern[1]].innerText;
-    let pos3value =      boxes[pattern[2]].innerText;
-    if(pos1value !== " " && pos2value !== " " && pos3value !== " "){
-    if(pos1value === pos2value && pos2value === pos3value){
-        console.log("winner",pos1value);
-        showwinner(pos1value);
-        // document.querySelector("#msg").innerText=pos1value;
+}
+const drawgame = ()=>{
+    document.querySelector("#msg").innerText=`Draw Game`;
+    msgcon.classList.remove("hide");
+
+}
+// const checkwinner = () =>{
+     
+//     for( let pattern of winpatterns){
+//     let pos1value =      boxes[pattern[0]].innerText;
+//     let pos2value =      boxes[pattern[1]].innerText;
+//     let pos3value =      boxes[pattern[2]].innerText;
+//     // console.log(pos1value);
+//     if(pos1value !=="" && pos2value !=="" && pos3value !== ""){
+//     if(pos1value === pos2value && pos2value === pos3value){
+//         // console.log("winner",  pos2value);
+//         showwinner(pos2value);
+//         break;
+//     }
+   
+//     // document.querySelector("#msg").innerText=`Winner is ${pos2value}`;
+//     // msgcon.classList.remove("hide");
+// }
+
+
+// }
+// }
+const checkwinner = () => {
+    let draw = true;
+    for (let pattern of winpatterns) {
+        let pos1value = boxes[pattern[0]].innerText;
+        let pos2value = boxes[pattern[1]].innerText;
+        let pos3value = boxes[pattern[2]].innerText;
+        if (pos1value !== "" && pos2value !== "" && pos3value !== "") {
+            if (pos1value === pos2value && pos2value === pos3value) {
+                showwinner(pos2value);
+                return;
+            }
+        } else {
+            draw = false;
+        }
     }
-}
-}
-}
+    if (draw) {
+        drawgame();
+    }
+};
